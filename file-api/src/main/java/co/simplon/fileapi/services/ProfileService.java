40e304920fb3acf79c3f25157686d41466283d16
damlaSub <96023986+damlaSub@users.noreply.github.com>
2023-04-10 19:32:14 +0200

    Delete file-api directory

--- file-api/src/main/java/co/simplon/fileapi/services/ProfileService.java
+++
@@ -1,9 +0,0 @@
-package co.simplon.fileapi.services;
-
-import co.simplon.fileapi.dtos.ProfileUpdateDto;
-
-public interface ProfileService {
-
-    void update(ProfileUpdateDto inputs, Long id);
-
-}
